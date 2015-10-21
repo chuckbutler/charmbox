57427255885fd5374844d15f41420a4036aaa2f4
Charles Butler <chuck@dasroot.net>
2015-10-21 19:13:28 -0400

    Install 1.25 for testing

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,23 @@
-FROM jujusolutions/jujubox:latest
-
+FROM ubuntu:14.04.3
 VOLUME ["/home/ubuntu/.juju", "/home/ubuntu/trusty", "/home/ubuntu/precise"]
-RUN apt-get update -qy
-RUN apt-get install -qy gcc cython git make
+
+ENV HOME=/home/ubuntu
+ENV GOPATH=/home/ubuntu/go
+ENV GOROOT=/usr/lib/go
+
+RUN apt-get update -qy && apt-get -y --no-install-recommends install \
+bzr \
+ca-certificates \
+git \
+golang-go \
+golang-src \
+mercurial \
+make \
+openssh-client \
+cython \
+gcc
+
 ADD install-review-tools.sh /install-review-tools.sh
 RUN /install-review-tools.sh
+ADD run.sh /run.sh
 CMD /run.sh
--- install-review-tools.sh
+++ install-review-tools.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 set -e
+
+useradd -m ubuntu
+echo "ubuntu ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/juju-users
 HOME=/home/ubuntu
 
 sudo apt-get update -qqy
 sudo apt-get install -qy unzip build-essential charm-tools python-dev python-pip python-virtualenv rsync
 sudo pip install bundletester flake8
 
+mkdir -p $HOME/.go
+export GOPATH=$HOME/.go
+export GOROOT=/usr/lib/go
+
+# Fetch latest code
+go get launchpad.net/godeps/...
+mkdir -p $HOME/.go/src/github.com/juju/
+git clone https://github.com/juju/juju $HOME/.go/src/github.com/juju/juju
+cd $HOME/.go/src/github.com/juju/juju
+git fetch --all
+git checkout 1.25
+
+# Build!
+JUJU_MAKE_GODEPS=true make godeps
+make build
+make install
+
+echo "export PATH=${HOME}/.go/bin:$PATH:${HOME}/.juju-plugins" >> ${HOME}/.bashrc
 chown -R ubuntu:ubuntu ${HOME}
+
